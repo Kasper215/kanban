66367968559dc0f246ce6abe0df91f1ab3a0b639
exxxar <exxxar@gmail.com>
2026-03-13 23:53:34 +0300

    update notification

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,7 +10,7 @@
 
     <link rel="manifest" href="/manifest.json?board_uuid={{\Illuminate\Support\Facades\Session::get("board_uuid")}}">
     <meta name="theme-color" content="#4f46e5">
-    <link rel="apple-touch-icon" href="/icons/icon-192.png">
+    <link rel="apple-touch-icon" href="/icons/icon-192x192.png">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="apple-mobile-web-app-title" content="Kanban">
