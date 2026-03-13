3800d2bc7518a7a76ed8cf94c8f3a5dbf6653b29
exxxar <exxxar@gmail.com>
2026-03-13 23:57:34 +0300

    update notification

--- app/Http/Controllers/ManifestController.php
+++ app/Http/Controllers/ManifestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Str;
 
 class ManifestController extends Controller
 {
@@ -13,6 +14,7 @@ class ManifestController extends Controller
         $boardUuid = session('board_uuid') ?? $request->query('board_uuid');
 
         $manifest = [
+            "id" => $boardUuid ?? Str::uuid(),
             "name" => "KanbanCRM",
             "short_name" => "Kanban",
             "start_url" => $boardUuid
