5943493badb767b28a9bb9e54cbdbb8d9b2deb86
exxxar <exxxar@gmail.com>
2026-03-23 15:37:09 +0300

    add new theme

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -25,6 +25,7 @@
     <link rel="shortcut icon" href="favicon.svg">
     <!-- Fonts -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/theme16.bootstrap.min.css">
 
 
     <style>
